f1672b69da044fb094ae2c343fc9f2e4851c075d
UsUsStudios <ususstudios1@gmail.com>
2026-07-16 17:13:33 -0600

    kernel.lua

--- system/boot/cfg/boot.lua
+++ system/boot/cfg/boot.lua
@@ -1,15 +1,15 @@
 return {
 	["Config"] = {
 		["DefaultEntry"] = 1, -- the option that appears highlighted first when entering the boot menu
-		["Autoboot"] = "none", -- or: index number for .Bootlist[i]
+		["Autoboot"] = 1, -- or: index number for .Bootlist[i]
 	},
 	["Bootlist"] = {
-		-- {
-		-- 	["OS Name"] = "NEEnix",
-		-- 	["OS Version"] = "0.0.1",
-		-- 	["OS Description"] = "NEATO-compatible UNIX-like system",
-		-- 	["OS Boot Path"] = "0:system:",
-		-- },
+		{
+			["OS Name"] = "NEEnix",
+			["OS Version"] = "0.0.1",
+			["OS Description"] = "NEATO-compatible UNIX-like system",
+			["OS Boot Path"] = "0:system:boot/kernel.lua",
+		},
 		{
 			["OS Name"] = "OS Installer",
 			["OS Version"] = "1.0.0", -- semver style (possibly?) enforced
