12860504a3f1fbfdef74835835903a6917e4a478
UsUsStudios <ususstudios1@gmail.com>
2026-07-19 15:24:39 -0600

    init script

--- system/boot/kernel/kernel.lua
+++ system/boot/kernel/kernel.lua
@@ -9,6 +9,7 @@ local scheduler = include("scheduler.lua")()
 
 scheduler.new_process(function()
 	coroutine.yield({ type = "mount", mountpoint = "/", fspath = "system:/boot/kernel/fs/rootfs" })
+	coroutine.yield({ type = "spawn", path = "/etc/initd/init.lua" })
 end)
 
 while true do
