b96f33fdfdf29a4754601012fd2295b1b7ca803b
UsUsStudios <ususstudios1@gmail.com>
2026-07-24 10:55:43 -0600

    error handling in neatobios

--- neatobios/bios.lua
+++ neatobios/bios.lua
@@ -89,15 +89,27 @@ local function launchBootOption(bootOption)
 	end
 
 	local args = bootOption["OS Args"]
-	local OS = loadfile(bootOption["OS Boot Path"], nil, env)
-	if args == nil then
-		OS()
-	elseif type(args) == "table" then
-		OS(table.unpack(args))
-	else
-		OS(args)
+	local OS, err = loadfile(bootOption["OS Boot Path"], nil, env)
+	if err then
+		font.drawLine(3, 3 + fontHeight * 1, err)
+		screen.draw()
+		for _ = 0, 10 do
+			coroutine.yield()
+		end
+		error(err)
+	elseif OS then
+		if args == nil then
+			OS()
+		elseif type(args) == "table" then
+			OS(table.unpack(args))
+		else
+			OS(args)
+		end
+		chip.shutdown()
 	end
-	chip.shutdown()
+	print("OS not available: " .. bootOption["OS Boot Path"])
+	font.drawLine(3, 3 + fontHeight * 1, "OS not available: " .. bootOption["OS Boot Path"])
+	screen.draw()
 end
 
 local function loadBootCfg()
@@ -197,5 +209,4 @@ local function benchmark()
 end
 
 init()
-benchmark()
 main()
